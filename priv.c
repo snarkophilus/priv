25a9adef89f8c38d318b4d7049f8e74cd6f80f7c
Simon Burge <github@thistledown.com.au>
1996-12-20 04:24:15 +0000

    Update copyrights.

--- priv.c
+++ priv.c
@@ -1,13 +1,40 @@
-/*	$Id: priv.c,v 1.20 1996/08/09 05:07:20 simonb Exp $
+/*	$Id: priv.c,v 1.21 1996/10/28 03:55:18 simonb Exp $
  *
  *	priv	run a command as a given user
  *
  *	Loosely based on a command called priv by:
  *		Ron Kuris, Dec 1988.
  *		Dan Busarow, DPC Systems, 22/11/91.
+ */
+
+/*
+ * Copyright (c) 1996 Telstra Corporation Limited. All rights reserved.
+ * Author: Simon Burge <simonb@telstra.com.au>
  *
- *	Copyright (c) 1996, Telstra Limited.  All Right Reserved.
- *	Author: Simon Burge, <simonb@telstra.com.au>
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by Simon Burge, Telstra Corp.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  */
 
 /*
@@ -19,7 +46,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: priv.c,v 1.20 1996/08/09 05:07:20 simonb Exp $";
+static char rcsid[] = "$Id: priv.c,v 1.21 1996/10/28 03:55:18 simonb Exp $";
 #endif /* not lint */
 
 #include <stdio.h>
